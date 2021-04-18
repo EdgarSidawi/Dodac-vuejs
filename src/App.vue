50f876f26f9cdf2794f5745332f17bcf182bf097
Edgar <romantibuai@gmail.com>
2021-04-18 16:01:13 +0100

    created navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     
 
     <v-main>
-      <router-view></router-view>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
   </v-app>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-    <v-btn class="pink white--text" elevation="2"
+
+    <v-btn class="pink white--text " elevation="2"
     >Click Me
     </v-btn>
-    <v-btn>Hi there</v-btn>
-    <v-btn>Hi there</v-btn>
+    <v-btn  color="pink" dark>Hi there</v-btn>
+    <v-btn depressed>Hi there</v-btn>
+    
+    <v-btn color="pink white--text"  depressed>
+      <v-icon left>mdi-email</v-icon>Email me
+    </v-btn>
+
+    <v-btn color="pink white--text"  large>
+      <v-icon left>mdi-email</v-icon>Email me
+    </v-btn> <span> </span>
+
+    <v-btn fab color="pink white--text" >
+      <v-icon>mdi-cards-heart</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
