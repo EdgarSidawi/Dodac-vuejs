c720f1a5c2b86d03fafcd613ee5ea9239eae85fb
Edgar <romantibuai@gmail.com>
2021-04-19 05:42:10 +0100

    added app to app bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,20 @@
 <template>
     <nav>
-        <v-navigation-drawer app class="blue" v-model="drawer">
-            <h1>navbar</h1>
-        </v-navigation-drawer>
-
-
-        <v-toolbar >
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-            <v-toolbar-title app >
+        <v-app-bar class="white" app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>
                 <span class="blue--text">D</span>
                 <span class="grey--text">O</span>
                 <span class="green--text">D</span>
                 <span class="red--text">A</span>
                 <span class="blue--text">C</span>
             </v-toolbar-title>
-        </v-toolbar>
+        </v-app-bar>
 
-        
+        <v-navigation-drawer app class="blue" v-model="drawer">
+            <h1>navbar</h1>
+        </v-navigation-drawer>
+
     </nav>
 </template>
 
