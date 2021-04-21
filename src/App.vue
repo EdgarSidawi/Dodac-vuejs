e5b90fbed8cc015f03bc925dcf0429de9097a4cf
Edgar <romantibuai@gmail.com>
2021-04-21 15:45:43 +0100

    login component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,33 @@
 <template>
   <v-app>
-    <Navbar/>
+    <div>
+      <Login/>
+    </div>
 
+    <div v-if="!login">
+    <Navbar />
     <v-main>
       <v-container class="mx-4" fluid>
         <router-view></router-view>
       </v-container>
     </v-main>
+    </div>
   </v-app>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Login from './components/login/Login'
 
 export default {
   name: 'App',
 
   components: {
-    Navbar,
+    Navbar,Login
   },
 
   data: () => ({
-    //
+    login: true
   }),
 };
 </script>
---
+++ src/components/login/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container class="grey lighten-4" fluid>
+      <v-layout>
+        <v-flex>
+            <v-form>
+                <v-card max-width="400" class="mx-auto pb-4" shaped>
+                    <v-card-title class="blue text-center">
+                        <h3 class="text-center">
+                             Login Form
+                        </h3> 
+                    </v-card-title>
+                    <v-card-text>
+                        <v-text-field label="username" prepend-icon=""></v-text-field>
+                        <v-text-field></v-text-field>
+                    </v-card-text>
+                    <v-card-action class="px-3">
+                        <v-btn rounded class="blue white--text">
+                            Login
+                        </v-btn>
+                    </v-card-action>
+
+                </v-card>
+            </v-form>
+        </v-flex>
+      </v-layout>
+    </v-container>
+</template>
