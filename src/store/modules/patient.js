af5176e67a9efe11ecabf1058360c03ff9ca727c
Edgar <romantibuai@gmail.com>
2021-04-30 13:09:58 +0100

    error state and mutation

--- src/store/modules/patient.js
+++ src/store/modules/patient.js
@@ -4,17 +4,24 @@ const patient = {
     namespaced: true,
 
     state: {
-        patients: []
+        patients: [],
+        error: ""
     },
     getters: {
     },
     mutations: {
+        SETPATIENTS(state, payload) {
+            state.patients = payload
+        },
+
+        ERROR(state) {
+            state.error = "Error connecting to database. Please try again later!"
+        }
 
     },
     actions: {
         searchPatient({ commit }, data) {
             axios.post('/searchPatient', data).then(res => {
-                console.log('patient: ', res);
                 commit();
             }).catch(err => console.log(err))
         }
