cb46ebbec5e409e7dbd6a9b2ebe3ce9e34681ea4
Edgar <romantibuai@gmail.com>
2021-05-12 13:28:01 +0100

    searchstaff actions and mutations

--- src/store/modules/staff.js
+++ src/store/modules/staff.js
@@ -1,12 +1,34 @@
-// import axios from "axios"
+import axios from "axios"
 
 const staff = {
     namespaced : true,
 
-    state: {},
-    getters: {},
-    mutations: {},
-    actions: {},
+    state: {
+        staffs: [],
+        loading: false
+    },
+    getters: {
+        staffs: (state) => state.staffs,
+        loading: (state) => state.loading
+    },
+    mutations: {
+        SETSTAFFS(state, payload){
+            state.staffs = payload
+        },
+        LOADING(state, payload){
+            state.loading = payload
+        }
+    },
+    actions: {
+        searchStaff({ commit }, data) {
+            commit("LOADING", true)
+            axios.post("/searchStaff", data).then(res => {
+                console.log(res)
+                commit("LOADING", false)
+                commit("SETSTAFFS", res.data.data)
+            })
+        }
+    },
 }
 
 export default staff
