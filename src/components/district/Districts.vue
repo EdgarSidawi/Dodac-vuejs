f6774e3862a29671f065c87f08e9261e24359010
Edgar <romantibuai@gmail.com>
2021-05-19 20:19:34 +0100

    loading mutation

--- src/components/district/Districts.vue
+++ src/components/district/Districts.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <v-layout>
+        <v-layout >
             <v-flex>
                 <v-dialog
                 v-model="districtInfo.dialog" 
@@ -12,13 +12,18 @@
                     <v-btn icon dark @click="districtInfo.dialog = false">
                         <v-icon>mdi-close</v-icon>
                     </v-btn>
-                    <v-toolbar-title>Close {{ created }}</v-toolbar-title>
+                    <v-toolbar-title>Close</v-toolbar-title>
                     </v-toolbar>
 
                     <v-card class="pa-4 mb-3 text-center">
                         <h3 class="pt-2">{{districtInfo.region}}</h3>
                         <hr class="grey mx-auto mb-7" width="700"/>
 
+                        <div class="text-center">
+                        <v-btn v-if="loading" class="white blue--text mx-auto" depressed :loading="loading">loading</v-btn>
+                        </div>
+
+                        <div v-if="districts.length > 0">
                         <v-card class="pa-4 mb-3 mx-10" v-for="district in districts" :key="district.id">
                         <v-layout row wrap justify-space-between>
                             <v-flex sm6 xs12 md4>
@@ -31,6 +36,10 @@
                             </v-flex>
                         </v-layout>
                         </v-card>
+                        </div>
+                        <div v-else>
+                            <h3 class="red--text pt-10">No Districts to display for {{districtInfo.region}}</h3>
+                        </div>
                     </v-card>
                 </v-dialog>
             </v-flex>
@@ -46,19 +55,7 @@ export default {
     props: ["districtInfo"],
     
     computed: {
-        ...mapGetters("district", ["districts"]),
-
-        created(){ 
-            if (this.districtInfo.dialog) {
-                this.getDistricts(this.districtInfo.id); 
-                console.log("true")
-                return true
-            }else {
-                console.log("false")
-                return false
-             }
-        },
-
+        ...mapGetters("district", ["districts", "loading"]),
     },
     methods: {
         ...mapState("district", ["getDistricts"]),
--- src/store/modules/district.js
+++ src/store/modules/district.js
@@ -4,19 +4,26 @@ const district = {
     namespaced: true,
 
     state: {
-        districts: []
+        districts: [],
+        loading: false
     },
     getters: {
-        districts: (state) => state.districts
+        districts: (state) => state.districts,
+        loading: (state) => state.loading
     },
     mutations: {
         GETDISTRICTS(state, payload) {
             state.districts = payload
+        },
+        LOADING(state, payload) {
+            state.loading = payload
         }
     },
     actions: {
-        getDistricts({ commit }, id) { 
+        getDistricts({ commit }, id) {
+            commit("LOADING", true)
             axios.get(`region/${id}/district`).then(res => {
+                commit("LOADING", false)
                 commit("GETDISTRICTS", res.data.data)
             })
         }
