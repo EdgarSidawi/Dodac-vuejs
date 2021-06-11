7e3ecf0385d1340cfa78291449d4187a444845a8
Edgar <romantibuai@gmail.com>
2021-06-11 13:49:33 +0100

    Loading mutation

--- src/store/modules/disease.js
+++ src/store/modules/disease.js
@@ -14,11 +14,16 @@ const disease = {
     mutations: {
         GETDISEASES(state, payload) {
             state.diseases = payload
+        },
+        LOADING(state, payload) {
+            state.loading = payload
         }
     },
     actions: {
         getDiseases({ commit }, data) {
+            commit("LOADING", true)
             axios.get(`district/${data}/disease`).then(res => {
+                commit("LOADING", false)
                 commit("GETDISEASES", res.data.data) 
             })
         }
