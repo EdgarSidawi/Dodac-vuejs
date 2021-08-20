53e239aac94354cf2f04a0b4f528db4a558816d0
Edgar <romantibuai@gmail.com>
2021-08-20 14:49:32 +0100

    update current disease

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,129 +1,137 @@
 <template>
-    <v-container  fluid>
-        
-        <h1 class="text-center display-3 ma-4">    
-            <span class="grey--text caption">Welcome to</span>
-            <br>
-            <span class="blue--text">D</span>
-            <span class="grey--text">O</span>
-            <span class="green--text">D</span>
-            <span class="red--text">A</span>
-            <span class="blue--text">C</span>
-        </h1>
-        <v-layout>
-            <v-flex>
-                <v-form>
-                    <v-card max-width="400" 
-                        class="mx-auto pb-4" 
-                        shaped
-                        :loading="isLoading"
-                        loader-height="10"
-                        >
-                        <v-card-title class="blue text-center">
-                            <h3 class="text-center mx-auto white--text">
-                                Login Form
-                            </h3> 
-                        </v-card-title>
-                        <v-alert v-if="error" class="caption ma-2" color="red" text dense outlined>
-                        <p class="red--text my-auto " align="center">{{error}}</p>
-                        </v-alert>
-                        <v-alert v-if="errorMessage" class="caption ma-2" color="red" text dense outlined>
-                        <p class="red--text my-auto " align="center">{{errorMessage}}</p>
-                        </v-alert>
-                        <v-card-text class="pt-1" >
-                            <v-text-field 
-                                v-model="form.username" 
-                                :rules="usernameRules"
-                                label="username" 
-                                prepend-icon="mdi-account" 
-                                >
-                            </v-text-field>
-                            <v-text-field 
-                                v-model="form.password" 
-                                :rules="passwordRules"
-                                label="password" 
-                                prepend-icon="mdi-account-lock"
-                                type="password"
-                                >
-                            </v-text-field>
-                            <v-select 
-                                :rules="districtRules"
-                                :items="items" 
-                                v-model="districtInfo.district"
-                                prepend-icon="mdi-city"
-                            ></v-select>
-                        </v-card-text>
-                        <v-card-actions class="px-10">
-                            <v-btn rounded class="blue white--text caption" @click.stop="signIn">
-                            Login 
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-form>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container fluid>
+    <h1 class="text-center display-3 ma-4">
+      <span class="grey--text caption">Welcome to</span>
+      <br />
+      <span class="blue--text">D</span>
+      <span class="grey--text">O</span>
+      <span class="green--text">D</span>
+      <span class="red--text">A</span>
+      <span class="blue--text">C</span>
+    </h1>
+    <v-layout>
+      <v-flex>
+        <v-form>
+          <v-card
+            max-width="400"
+            class="mx-auto pb-4"
+            shaped
+            :loading="isLoading"
+            loader-height="10"
+          >
+            <v-card-title class="blue text-center">
+              <h3 class="text-center mx-auto white--text">Login Form</h3>
+            </v-card-title>
+            <v-alert v-if="error" class="caption ma-2" color="red" text dense outlined>
+              <p class="red--text my-auto" align="center">{{ error }}</p>
+            </v-alert>
+            <v-alert
+              v-if="errorMessage"
+              class="caption ma-2"
+              color="red"
+              text
+              dense
+              outlined
+            >
+              <p class="red--text my-auto" align="center">{{ errorMessage }}</p>
+            </v-alert>
+            <v-card-text class="pt-1">
+              <v-text-field
+                v-model="form.username"
+                :rules="usernameRules"
+                label="username"
+                prepend-icon="mdi-account"
+              >
+              </v-text-field>
+              <v-text-field
+                v-model="form.password"
+                :rules="passwordRules"
+                label="password"
+                prepend-icon="mdi-account-lock"
+                type="password"
+              >
+              </v-text-field>
+              <v-select
+                :rules="districtRules"
+                :items="items"
+                v-model="districtInfo.district"
+                prepend-icon="mdi-city"
+              ></v-select>
+            </v-card-text>
+            <v-card-actions class="px-10">
+              <v-btn rounded class="blue white--text caption" @click.stop="signIn">
+                Login
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import { mapGetters,mapActions} from 'vuex'
+import { mapGetters, mapActions } from "vuex";
 
 export default {
-        data() {
-            return{
-                form: {
-                    username:'',
-                    password:'',
-                },
-                districtInfo:{
-                    district: '',
-                    district_id: null,
-                },
-                error: '',
-                usernameRules: [ v => !!v || 'Username is required'],
-                passwordRules: [ v => !!v || 'Password is required'],
-                districtRules: [ v => !!v || 'district is required']
-            }
-        },
-        computed: {
-            ...mapGetters('auth',['isLoggedIn','errorMessage','isLoading']),
-            ...mapGetters('district',['allDistricts']),
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+      districtInfo: {
+        district: "",
+        district_id: null,
+      },
+      error: "",
+      usernameRules: [(v) => !!v || "Username is required"],
+      passwordRules: [(v) => !!v || "Password is required"],
+      districtRules: [(v) => !!v || "district is required"],
+    };
+  },
+  computed: {
+    ...mapGetters("auth", ["isLoggedIn", "errorMessage", "isLoading"]),
+    ...mapGetters("district", ["allDistricts"]),
 
-            items() {
-                if(this.allDistricts){
-                    return this.allDistricts.map(item =>{
-                        return item.name
-                    })
-                }else{
-                    return []
-                }
-            },
-        },
-        methods: {
-            ...mapActions('auth',['login']),
-            ...mapActions('district', ['getAllDistricts']),
+    items() {
+      if (this.allDistricts) {
+        return this.allDistricts.map((item) => {
+          return item.name;
+        });
+      } else {
+        return [];
+      }
+    },
+  },
+  methods: {
+    ...mapActions("auth", ["login"]),
+    ...mapActions("district", ["getAllDistricts"]),
 
-            signIn(){
-                if(this.form.username === "" || this.form.password === "" || this.districtInfo.district === ""){                   
-                    this.error = "username, password and district cannot be empty!"
+    signIn() {
+      if (
+        this.form.username === "" ||
+        this.form.password === "" ||
+        this.districtInfo.district === ""
+      ) {
+        this.error = "username, password and district cannot be empty!";
+      } else {
+        for (var i in this.allDistricts) {
+          if (this.allDistricts[i].name == this.districtInfo.district) {
+            this.districtInfo.district_id = this.allDistricts[i].id;
+            break;
+          }
+        }
+        localStorage.setItem("district", this.districtInfo.district);
+        localStorage.setItem("district_id", this.districtInfo.district_id);
 
-                }else{
-                    for (var i in this.allDistricts) { 
-                        if (this.allDistricts[i].name == this.districtInfo.district) {
-                        this.districtInfo.district_id = this.allDistricts[i].id
-                        break 
-                        } 
-                    }
-                    localStorage.setItem('district', this.districtInfo.district)
-                    localStorage.setItem('district_id', this.districtInfo.district_id)
-
-                    this.error= ""
-                    this.login(this.form) 
-                }
-            }
-        },
-        created(){ 
-            this.getAllDistricts()
-        },
-}
+        this.error = "";
+        this.login(this.form);
+      }
+    },
+  },
+  created() {
+    this.getAllDistricts();
+  },
+};
 </script>
--- src/components/patient/EditPatient.vue
+++ src/components/patient/EditPatient.vue
@@ -1,123 +1,135 @@
 <template>
-    <div>
-        <v-layout>
-            <v-flex>
-                <v-dialog
-                v-model="patientInfo.dialog"
-                persistent
-                max-width="600px"
-                eager
-                >
-                    <v-card>
-                        <v-card-title class="red text-center mx-auto">
-                            <h3 class="mx-auto white--text text-center"> Edit Patient</h3>
-                        </v-card-title>
-                        <v-card-text>
-                            <v-text-field
-                                v-model="form.firstName"
-                                label = "First Name"
-                            >
-                            </v-text-field>
-                            <v-text-field
-                                v-model="form.lastName"
-                                label = "Last Name"
-                            >
-                            </v-text-field>
-                            <div class="caption">Date Of Birth</div>
-                            <v-date-picker 
-                            v-model="form.dateOfBirth"
-                            >
-                            </v-date-picker>
-                            <v-text-field
-                                v-model="form.allergies"
-                                label = "Allergies"
-                            >
-                            </v-text-field>
-                            <v-text-field
-                                :value = "patientInfo.patient.last_disease_diagnosed"
-                                label = "Last Disease Diagnosed"
-                                disabled
-                            >
-                            </v-text-field>
-                            <v-select 
-                                v-if="role == 'Doctor'"
-                                label = "Current Disease Diagnosed"
-                                :items="items" 
-                                v-model="form.last_disease_diagnosed"
-                                prepend-icon="mdi-virus"
-                            ></v-select>
-                        </v-card-text>
-                        <v-card-actions>
-                            <h4 v-if="notify" class="green--text ml-16 pl-16">Updated Successfully</h4>
-                            <v-spacer></v-spacer>
-                            <v-btn class="red white--text" @click="close">Close</v-btn>
-                            <v-btn class="blue white--text" :disabled="disable" @click="update(patientInfo.patient.id)">Update</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-            </v-flex>
-        </v-layout>
-   
-
-    </div>
+  <div>
+    <v-layout>
+      <v-flex>
+        <v-dialog v-model="patientInfo.dialog" persistent max-width="600px" eager>
+          <v-card>
+            <v-card-title class="red text-center mx-auto">
+              <h3 class="mx-auto white--text text-center">Edit Patient</h3>
+            </v-card-title>
+            <v-card-text>
+              <v-text-field v-model="form.firstName" label="First Name"> </v-text-field>
+              <v-text-field v-model="form.lastName" label="Last Name"> </v-text-field>
+              <div class="caption">Date Of Birth</div>
+              <v-date-picker v-model="form.dateOfBirth"> </v-date-picker>
+              <v-text-field v-model="form.allergies" label="Allergies"> </v-text-field>
+              <v-text-field
+                :value="patientInfo.patient.last_disease_diagnosed"
+                label="Last Disease Diagnosed"
+                disabled
+              >
+              </v-text-field>
+              <v-select
+                v-if="role == 'Doctor'"
+                label="Current Disease Diagnosed"
+                :items="items"
+                v-model="form.last_disease_diagnosed"
+                prepend-icon="mdi-virus"
+              ></v-select>
+            </v-card-text>
+            <v-card-actions>
+              <h4 v-if="notify" class="green--text ml-16 pl-16">Updated Successfully</h4>
+              <v-spacer></v-spacer>
+              <v-btn class="red white--text" @click="close">Close</v-btn>
+              <v-btn
+                class="blue white--text"
+                :disabled="disable"
+                @click="update(patientInfo.patient.id)"
+                >Update
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-flex>
+    </v-layout>
+  </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex"
+import { mapActions, mapGetters } from "vuex";
 
 export default {
-    props:["patientInfo"],
-    data(){
-        return{
-            form: {
-                id: this.patientInfo.patient.id,
-                firstName: this.patientInfo.patient.firstName,
-                lastName: this.patientInfo.patient.lastName,
-                dateOfBirth: this.patientInfo.patient.dateOfBirth,
-                allergies: this.patientInfo.patient.allergies,
-                last_disease_diagnosed: this.patientInfo.patient.last_disease_diagnosed,
-                last_visited_hospital: "2 seconds ago"
-            },
-            notify: false
-        }
-    },
-    created(){  
-        this.getDiseases(localStorage.getItem("district_id"))
-    },
-    computed: {        
-        ...mapGetters('disease', ['diseases']),
-        role(){
-            return localStorage.getItem('role')
-        },
-        disable(){ 
-            if(!this.form.firstName || !this.form.lastName || !this.form.dateOfBirth || !this.form.allergies) { 
-            return true 
-            }else{ 
-            return false 
-            }
-         }
-    },
-    methods: {
-        ...mapActions('patient',['updatePatient']),
-        ...mapActions('disease',['getDiseases']),
+  props: ["patientInfo"],
+  data() {
+    return {
+      form: {
+        id: this.patientInfo.patient.id,
+        firstName: this.patientInfo.patient.firstName,
+        lastName: this.patientInfo.patient.lastName,
+        dateOfBirth: this.patientInfo.patient.dateOfBirth,
+        allergies: this.patientInfo.patient.allergies,
+        last_disease_diagnosed: "",
+        last_visited_hospital: "2 seconds ago",
+      },
+      notify: false,
+    };
+  },
+  created() {
+    this.getDiseases(localStorage.getItem("district_id"));
+  },
+  computed: {
+    ...mapGetters("disease", ["diseases"]),
 
-        close(){
-            this.patientInfo.dialog = false,
-            this.patientInfo.edit =false
-        },
-        update(id){
-            var data = {
-                form: this.form,
-                id: id
-            }
-            this.updatePatient(data) 
-    
-            this.notify = true
-            setTimeout(() => {
-                this.notify=false
-            }, 2000);
+    role() {
+      return localStorage.getItem("role");
+    },
+    disable() {
+      if (
+        !this.form.firstName ||
+        !this.form.lastName ||
+        !this.form.dateOfBirth ||
+        !this.form.allergies
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    items() {
+      if (this.diseases) {
+        return this.diseases.map((item) => {
+          return item.name;
+        });
+      } else {
+        return [];
+      }
+    },
+  },
+  methods: {
+    ...mapActions("patient", ["updatePatient"]),
+    ...mapActions("disease", ["getDiseases", "updateDisease"]),
+
+    close() {
+      (this.patientInfo.dialog = false), (this.patientInfo.edit = false);
+    },
+
+    update(id) {
+      var data = {
+        form: this.form,
+        id: id,
+      };
+      this.updatePatient(data);
+
+      if (this.form.last_disease_diagnosed) {
+        for (var i in this.diseases) {
+          if (this.diseases[i].name == this.form.last_disease_diagnosed) {
+            this.diseases[i].current++;
+            var disease = {
+                form: this.diseases[i],
+                id: this.diseases[i].id,
+                district_id: this.diseases[i].district_id
+            };
+            this.updateDisease(disease);
+            break;
+          }
         }
-    }
-    
-}
+      }
+
+      this.notify = true;
+      setTimeout(() => {
+        this.notify = false;
+      }, 2000);
+    },
+  },
+};
 </script>
